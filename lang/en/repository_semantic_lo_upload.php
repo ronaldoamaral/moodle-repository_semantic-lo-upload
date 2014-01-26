a38f7b4c00c8849147f668cd978e3a1f941176a0
Ronaldo Amaral <rsantos@iff.edu.br>
2014-01-26 21:00:00 -0200

    Fist Commit

---
+++ lang/en/repository_semantic_lo_upload.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Strings for Semantic_LO repository plugin
+ *
+ */
+ 
+defined('MOODLE_INTERNAL') || die(); 
+ 
+$string['configplugin'] = 'Configuration for upload plugin';
+$string['pluginname_help'] = 'Upload a file to Moodle';
+$string['pluginname'] = 'Semantic LO upload';
+$string['semantic_lo_upload:view'] = 'Use uploading in file picker';
+$string['semantic_lo_service'] = 'Semantic LO REST Service';
+$string['semantic_lo_repository'] = 'Semantic LO Repository';
+$string['upload_error_ini_size'] = 'The uploaded file exceeds the upload_max_filesize directive in php.ini.';
+$string['upload_error_form_size'] = 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.';
+$string['upload_error_partial'] = 'The uploaded file was only partially uploaded.';
+$string['upload_error_no_file'] = 'No file was uploaded.';
+$string['upload_error_no_tmp_dir'] = 'PHP is missing a temporary folder.';
+$string['upload_error_cant_write'] = 'Failed to write file to disk.';
+$string['upload_error_extension'] = 'A PHP extension stopped the file upload.';
+$string['upload_error_invalid_file'] = 'The file \'{$a}\' is either empty or a folder. To upload folders zip them first.';
